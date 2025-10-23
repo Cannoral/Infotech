2ef551bacd5c4556201ebfe2f9022c126480252d
Zernov Oleg <olegfact@gmail.com>
2025-10-23 10:04:53 +0000

    Обновленный composer.lock

--- src/config/web.php
+++ src/config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'zV5Ti25iuJrz3pfLJXdPyH6WxP-PyHGl',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
